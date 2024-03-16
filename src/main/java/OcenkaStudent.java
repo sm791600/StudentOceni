32aef1510af69ac3e07a5f946d308413ab47fac1
sm791600 <stojanovikmarko7916@gmail.com>
2024-03-16 21:11:54 +0100

    initialisation

---
+++ src/main/java/OcenkaStudent.java
@@ -0,0 +1,21 @@
+/*Од тастатура се внесува максимален број на поени кои студентот може да ги
+добие на тестот кој го решавал, во зависност од поените според формулата (оценка =
+освоени бодови/максимален број на бодови) * 100, на екран да се испечати која
+оценка ја добил студентот, доколку се внесе поголем број на поени од максималниот
+број соодветно да се постапи.  */
+import java.util.Scanner;
+public class OcenkaStudent {
+
+	public static void main(String[] args) {
+		int maxP, osvP;
+		System.out.println("Vnesi maksimum bodovi koi moze da se osvojat na testot\n");
+		Scanner tastatura =new Scanner(System.in);
+		maxP=tastatura.nextInt();
+		System.out.println("Vnesi osvoeni bodovi\n");
+		osvP=tastatura.nextInt();
+		
+		
+		
+	}
+
+}
