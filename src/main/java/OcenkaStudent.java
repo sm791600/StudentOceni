01865ff8e777469de77e2a5fbfadb63e89c1c0ef
sm791600 <stojanovikmarko7916@gmail.com>
2024-03-16 21:40:11 +0100

    greskata e popraveno

--- src/main/java/OcenkaStudent.java
+++ src/main/java/OcenkaStudent.java
@@ -7,29 +7,36 @@ import java.util.Scanner;
 public class OcenkaStudent {
 
 	public static void main(String[] args) {
-		int maxP=0, osvP=0, procent=0;
+		double maxP=0, osvP=0, procent=0;
 		System.out.println("Vnesi maksimum bodovi koi moze da se osvojat na testot\n");
 		Scanner tastatura =new Scanner(System.in);
-		maxP=tastatura.nextInt();
+		maxP=tastatura.nextDouble();
 		System.out.println("Vnesi osvoeni bodovi\n");
-		osvP=tastatura.nextInt();
-		procent=osvP/maxP*100;
-		if(procent>maxP) {
+		osvP=tastatura.nextDouble();
+		procent=(osvP/maxP)*100;
+		System.out.println("osvoeni se "+osvP+" od "+ maxP+" znaci ima "+procent+"%");
+		if(osvP>maxP) {
 			System.out.println("Osvoeni se povekje od maksimalnata kolicina bodovi");
 			System.exit(0);
 			}
 		if(procent<51){
 			System.out.println("studentot padnal");
-		} else if(procent<=60 && procent =>51){
+			
+		} else if(procent<=60 && procent >=51){
 			System.out.println("studentot dobil 6");
-		}else if(procent<=70 && procent =>51){
+			
+		}else if(procent<=70 && procent >=61){
 			System.out.println("studentot dobil 7");
-		}else if(procent<=80 && procent =>51){
+			
+		}else if(procent<=80 && procent >=71){
 			System.out.println("studentot dobil 8");
-		}else if(procent<=90 && procent =>51){
+			
+		}else if(procent<=90 && procent >=81){
 			System.out.println("studentot dobil 9");
+			
 		}else if(procent>90 && procent <=100){
 			System.out.println("studentot dobil 10");
+			
 		}
 		
 	}
